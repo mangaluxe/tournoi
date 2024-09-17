c4c02c18bf22cb3b68f94b060e00006f6d235283
Mangaluxe <nomail@laposte.net>
2024-09-17 08:16:13 +0200

    debug

--- src/main/java/org/example/tournoi/dao/InscriptionRepository.java
+++ src/main/java/org/example/tournoi/dao/InscriptionRepository.java
@@ -3,9 +3,15 @@ package org.example.tournoi.dao;
 import org.example.tournoi.entity.Inscription;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 
 public interface InscriptionRepository extends JpaRepository<Inscription, Integer> {
 
     // Les méthodes de base : save(), delete(), findAll() sont natives
+
+    List<Inscription> findByUtilisateurId(int utilisateurId);
+
+    List<Inscription> findByTournoiId(int tournoiId);
 
 }
--- src/main/java/org/example/tournoi/service/InscriptionService.java
+++ src/main/java/org/example/tournoi/service/InscriptionService.java
@@ -2,6 +2,8 @@ package org.example.tournoi.service;
 
 import org.example.tournoi.dao.InscriptionRepository;
 import org.example.tournoi.entity.Inscription;
+import org.example.tournoi.entity.Tournoi;
+import org.example.tournoi.entity.Utilisateur;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,17 +18,43 @@ public class InscriptionService {
         this.inscriptionRepository = inscriptionRepository;
     }
 
-    // Créer une nouvelle inscription
+
+//    public List<Inscription> getInscriptionsParUtilisateur(int utilisateurId) {
+//        return inscriptionRepository.findByUtilisateurId(utilisateurId);
+//    }
+
+    public List<Inscription> getInscriptionsParTournoi(int tournoiId) {
+        return inscriptionRepository.findByTournoiId(tournoiId);
+    }
+
+
+    /**
+     * Créer une nouvelle inscription
+     */
     public void creerInscription(Inscription inscription) {
         inscriptionRepository.save(inscription);
     }
+//    public void creerInscription(Inscription inscription) {
+//        Utilisateur utilisateur = utilisateurService.getUtilisateurById(inscription.getUtilisateur().getId());
+//        Tournoi tournoi = tournoiService.getTournoiById(inscription.getTournoi().getId());
+//
+//        if (utilisateur != null && tournoi != null) {
+//            inscription.setUtilisateur(utilisateur);
+//            inscription.setTournoi(tournoi);
+//            inscriptionRepository.save(inscription);
+//        }
+//    }
 
-    // Obtenir toutes les inscriptions
+    /**
+     * Obtenir toutes les inscriptions
+     */
     public List<Inscription> getAllInscriptions() {
         return inscriptionRepository.findAll();
     }
 
-    // Supprimer une inscription
+    /**
+     * Supprimer une inscription
+     */
     public void supprimerInscription(int id) {
         inscriptionRepository.deleteById(id);
     }
